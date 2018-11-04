c5beb794ee6e39b704119f75bdfbb28d9dc6d229
Datman9001 <tcalvin571@gmail.com>
2018-11-04 11:18:07 -0600

    final stage

--- js/main.js
+++ js/main.js
@@ -1,31 +1,27 @@
-function batteryCharge(){
+function batteryCharge() {
     let battery = document.getElementById('battery');
-    
+    //battery begin full
     battery.innerHTML = '&#Xf240';
-    setTimeout(function(){
+    setTimeout(function () {
         battery.innerHTML = '&#xf244';
     }, 1000)
-    setTimeout(function(){
+    setTimeout(function () {
         battery.innerHTML = '&#xf243';
     }, 2000)
 
-    
-    setTimeout(function(){
+
+    setTimeout(function () {
         battery.innerHTML = '&#xf242';
     }, 3000);
 
-    setTimeout(function(){
-        battery.innerHTML ='&#xf241';
-
+    setTimeout(function () {
+        battery.innerHTML = '&#xf241';
     }, 4000);
-    
-        
- 
-} 
+
+
+
+}
 //animation call
 batteryCharge();
 //animation interval every 2 seconds
 setInterval(batteryCharge, 5000);
-
-
-// f241
